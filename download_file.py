5594830b65acbab333ca36ffa8064c5ad8c10e72
mrumair <umairbhatti779@gmail.com>
2024-03-24 21:29:44 +0500

    refactor code

--- download_file.py
+++ download_file.py
@@ -1,17 +1,19 @@
 import requests
 import validators
 import os, shutil
-import time
-import zipfile
+from glob import glob
 import concurrent.futures
 
 
-def fetchFile(url, file_path):
+def fetch_file(url, file_path):
     if validators.url(url):
         r = requests.get(url)
         if r.status_code == 200:
-            with open(file_path, 'wb') as w:
-                w.write(r.content)
+            if glob(file_path):
+                print('WARNING: File already exists')
+            else:
+                with open(file_path, 'wb') as w:
+                    w.write(r.content)
             return True
         else:
             return False
@@ -19,15 +21,15 @@ def fetchFile(url, file_path):
         False
 
 
-def getLocalFilePath(url, path_downloads):
+def get_local_file_path(url, path_downloads):
     file_name = url.split('/')[-1] 
     file_path = os.path.join(path_downloads, file_name)
     return file_path
 
 
-def downloadAFile(URL, path_downloads, workers):
+def download_file(URL, path_downloads, workers):
     with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
-        future_to_url = {executor.submit(fetchFile, URL, getLocalFilePath(URL,path_downloads)): (URL, getLocalFilePath(URL,path_downloads))}
+        future_to_url = {executor.submit(fetch_file, URL, get_local_file_path(URL,path_downloads)): (URL, get_local_file_path(URL,path_downloads))}
         for future in concurrent.futures.as_completed(future_to_url):
             url, file_path = future_to_url[future]
             print("Downloading file from: ", url)
@@ -35,19 +37,11 @@ def downloadAFile(URL, path_downloads, workers):
                 result = future.result()
                 if result:
                     print("File downloaded successfully and saved to:", path_downloads)
-                    #extractFile(file_path, path_downloads)
                 else:
                     print("FAILED to download file from: ", url)
             except Exception as exc:
                 print(f"FAILED to download file from {url}: {exc}")
 
-    
-# def extractFile(file_path, pathToExtract):
-#     with zipfile.ZipFile(file_path, 'r') as zip_ref:
-#         zip_ref.extractall(pathToExtract)      
-#     os.remove(file_path)
-#     shutil.rmtree(pathToExtract+'\__MACOSX')
-#     print("FILE IS READY")
 
 
 def main(url, path_downloads):
@@ -56,7 +50,7 @@ def main(url, path_downloads):
     if not os.path.exists(path_downloads):
         os.mkdir(path_downloads)
     
-    downloadAFile(url, path_downloads, workers)
+    download_file(url, path_downloads, workers)
       
 
 # if __name__ == "__main__":
